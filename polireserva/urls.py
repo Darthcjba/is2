a93f886051526bbecec546059dd1ea2e4bec2b46
Elias Mora <eliamora94@gmail.com>
2017-04-25 03:37:14 -0400

    urls.py de polireserva, vista de Tipo de Recursos, template Tipo de Recursos

--- polireserva/urls.py
+++ polireserva/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /polireserva/
     url(r'^$', views.index, name='polindex'),
+    #/polireserva/tdr/
+    url(r'^tdr/$', views.tdrlist, name= 'tdrlist'),
+    #/polireserva/tdr/154/
+    url(r'^tdr/(?P<id_tdr>[0-9]+)/$', views.tdrdetail, name= 'tdrdetail'),
 ]
--- polireserva/views.py
+++ polireserva/views.py
@@ -1,8 +1,19 @@
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import TdRecurso
 
 @login_required(login_url='login/')
 def index(request):
     return render(request, 'principal/pagina_principal.html', {'current_user': request.user})
 
+def tdrlist(request):
+    all_tdr = TdRecurso.objects.all()
+    context = {
+        'all_tdr':all_tdr,
+    }
+    return render(request, 'tdr/list.html', context)
+
+
+def tdrdetail(request, id_tdr):
+    return HttpResponse("<h2>Detalles de Tipo de Recurso id: " + str(id_tdr) + "</h2>")
