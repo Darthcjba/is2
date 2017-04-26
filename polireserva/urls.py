8b56196bf22c4f7de0705f8c7e1d2266d231732d
Matias <matias.rt@hotmail.com>
2017-04-26 01:10:48 -0400

    Formulario de creacion de recursos agregado

--- polireserva/urls.py
+++ polireserva/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^tdr/$', views.tdrlist, name= 'tdrlist'),
     #/polireserva/tdr/<id_tdr>/
     url(r'^tdr/(?P<id_tdr>[0-9]+)/$', views.tdrdetail, name= 'tdrdetail'),
+    # /polireserva/tdr/<id_tdr>/newr
+    url(r'^tdr/(?P<id_tdr>[0-9]+)/new/$', views.recursofill, name='recursofill'),
     #/polireserva/tdr/newtdr/
     url(r'^tdr/new/$',views.tdrfill,name='tdrfill')
 
