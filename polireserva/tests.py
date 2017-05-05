c206b9edd20e46fb1c9f0a19a53faffb043c024e
Matias <matias.rt@hotmail.com>
2017-05-05 18:47:19 -0400

    Pruebas Unitarias Agregadas

--- polireserva/tests.py
+++ polireserva/tests.py
@@ -1,17 +1,11 @@
-from django.test import TestCase
 
-# Create your tests here.
-import datetime
-from django.contrib.auth.models import User, Group
-from django.contrib.auth.models import Permission
-from django.core.urlresolvers import reverse
+
 from django.test import TestCase
-from django.contrib.auth.models import User, Group, Permission
 from django.contrib.auth import SESSION_KEY
-from django.utils import timezone
-import reversion
-from project.models import Proyecto, Flujo, UserStory, Sprint, Actividad
-
+from polireserva.models import TdRecurso,Recurso
+from django.contrib.auth.models import User
+from rolepermissions.roles import assign_role
+from rolepermissions.checkers import has_permission,has_role
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -47,3 +41,45 @@ class LoginTest(TestCase):
         self.assertTrue(login)
         c.logout()
         self.assertTrue(SESSION_KEY not in self.client.session)
+
+
+
+class tdrTestCase(TestCase):
+    def setUp(self):
+        a1 = TdRecurso.objects.create(description="Aula")
+        a2 = TdRecurso.objects.create(description="Proyector")
+        Recurso.objects.create(name_r="A50", id_tdr=a1,description="objeto de prueba 1",date_c="2017-05-05",date_m="2017-05-05")
+        Recurso.objects.create(name_r="A51", id_tdr=a1,description="objeto de prueba 2",date_c="2017-05-05",date_m="2017-05-05")
+        Recurso.objects.create(name_r="epson", id_tdr=a2,description="objeto de prueba 3",date_c="2017-05-05",date_m="2017-05-05")
+
+    def test_recurso_tdr(self):
+        recurso1 = Recurso.objects.get(name_r="A51")
+        tipo= TdRecurso.objects.get(id_tdr=recurso1.id_tdr.id_tdr)
+        self.assertEqual(tipo.description, "Aula")
+
+
+class userTestCase(TestCase):
+    def setUp(self):
+        user=User.objects.create(username="Matirivas1995",first_name="Mati",last_name="Rivas",email="matias.rt@hotmail.com",password="123456qwe")
+        user1 = User.objects.create(username="Eliasmorag", first_name="Elias", last_name="Mora",email="emora@emora.com.py", password="altoqueroque")
+        assign_role(user,'administrador')
+        assign_role(user1,'usuario')
+
+
+    def test_has_permission(self):
+        usertest=User.objects.get(id=1)
+        usertest2=User.objects.get(id=2)
+        self.assertIs(has_permission(usertest,'can_access_admin'), True)
+        self.assertIs(has_permission(usertest2, 'can_access_admin'), False)
+
+    def test_has_role(self):
+        usertest = User.objects.get(id=1)
+        self.assertIs(has_role(usertest,'tecnico'),False)
+
+
+
+
+
+
+
+
